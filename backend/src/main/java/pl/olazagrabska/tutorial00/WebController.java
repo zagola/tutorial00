dd1402fadfeff0ac22312938ac7228047129fee1
Aleksandra Zagrabska <ozagrabska@gmail.com>
2022-07-14 18:52:23 +0200

    Add new endpoint "/content/{id}"
    New endpoint intended to display details about selected element.

--- backend/src/main/java/pl/olazagrabska/tutorial00/WebController.java
+++ backend/src/main/java/pl/olazagrabska/tutorial00/WebController.java
@@ -2,18 +2,31 @@ package pl.olazagrabska.tutorial00;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import java.util.Optional;
 
 @Controller
 public class WebController {
     @Autowired
     private ArticlesRepository repository;
+
     @GetMapping("/hello")
     public String helloWorld(Model model){
         Iterable<Element> elements = repository.findAll();
 
         model.addAttribute("elements", elements);
-        return "hello";
+        return "hello.html";
+    }
+    @GetMapping("/content/{id}")
+    public String getContentById(Model model, @PathVariable Character id) {
+        Optional<Element> element = repository.findById(id);
+        if (element.isPresent()) {
+            model.addAttribute("element", element.get());
+            return "content.html";
+        } else {
+            return "notfound.html";
+        }
     }
 }
